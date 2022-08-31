edfc80551d6bff109dce976dda03c1f17bab590b
José Jânio Dos Santos Silva <92950695+JanioSantos13659@users.noreply.github.com>
2022-08-31 16:08:59 -0300

    Delete loja directory

--- loja/src/main/java/br/com/loja/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.loja.repository;
-
-import br.com.loja.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
-    Cliente findBynome(String nome);
-}
