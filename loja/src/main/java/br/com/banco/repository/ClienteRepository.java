bdc4ec31a0d1ac3cc00c4182b21bebc8f1ec1ca7
José Jânio Dos Santos Silva <92950695+JanioSantos13659@users.noreply.github.com>
2022-09-01 23:29:25 -0300

    Delete loja directory

--- loja/src/main/java/br/com/banco/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.banco.repository;
-
-import br.com.banco.entity.Cliente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
-
-}
