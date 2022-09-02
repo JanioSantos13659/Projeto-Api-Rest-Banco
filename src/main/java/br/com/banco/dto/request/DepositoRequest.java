d363f7d6f501cd5d8607c14cda4fa25469dcfa5b
José Jânio Dos Santos Silva <92950695+JanioSantos13659@users.noreply.github.com>
2022-09-02 11:40:08 -0300

    Update DepositoRequest.java

--- src/main/java/br/com/banco/dto/request/DepositoRequest.java
+++ src/main/java/br/com/banco/dto/request/DepositoRequest.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Size;
 @Setter
 public class DepositoRequest {
 
-
+    @Positive
     private Double valorDeposito;
 
     @Size(min = 4, max = 11, message = "O campo numeroContaDeposito deve ter de 4 a 11 posicoes")
